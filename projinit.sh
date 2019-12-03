2b34ac6b087a7bcb81010a92b8a7c8935135a6a6
Alec Wong <aw685@cornell.edu>
2019-12-02 20:15:00 -0500

    Adding project init script

---
+++ projinit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make folders
+
+if [ ! -d R/ ]
+then
+	mkdir R/
+fi
+
+if [ ! -d output/ ]
+then
+	mkdir output/
+fi
+
+if [ ! -d data/ ]
+then
+	mkdir data/
+fi
+
+if [ ! -d report/ ]
+then
+	mkdir report/
+fi
+
