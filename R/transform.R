e87cdc53d0f612d512f8462505c37c4f7acc152a
Alec Wong <aw685@cornell.edu>
2019-12-02 22:05:46 -0500

    finish transforming data, add commentary

--- R/transform.R
+++ R/transform.R
@@ -8,6 +8,7 @@
   library(readxl)
   library(vroom)
   library(tidyr)
+  library(janitor)
 }
 
 source("R/transform_functions.R")
@@ -18,12 +19,26 @@ a5a = readxl::read_excel('data/a5a.xlsx', range = "A4:O58")
 
 # Data needs cleaning ----------------------------------------------------------
 
+# The headers are from merged columns, and so the values apply to multiple columns
 a5a_headers = a5a[1:2, 2:15]
+# The data are fine, but the labels could use work and the data are in wide form.
 a5a_data    = a5a[4:nrow(a5a),]
 
+# The goal here is to fill in the NA values using the previous (left) values,
+# and then combine down the columns to get an informative label for each col.
 a5a_headers[1,] = fill_left(a5a_headers[1,])
 a5a_headers     = merge_down(a5a_headers)
 
+# Now set these informative names to be the names of the data.
 names(a5a_data) = c(names(a5a_data)[1], a5a_headers)
 
-a5a_longer = pivot_longer()
+# Pivot the wide data into long data, and separate the "year_type" columns into two.
+a5a_longer = pivot_longer(a5a_data, cols = `2016_Total`:`1992_Citizen`, names_to = "year_type", values_to = "voting_rate")
+a5a_longer = separate(a5a_longer, col = year_type, into = c("year", "type"), remove = TRUE)
+a5a_longer = clean_names(a5a_longer)
+
+# Write to disk ----------------------------------------------------------------
+
+vroom_write(a5a_longer, path = 'output/a5a.tsv')
+
+# END --------------------------------------------------------------------------
