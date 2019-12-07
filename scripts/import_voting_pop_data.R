d2477c4c9bb07f5c4dd57e9ab81e1cab61942a89
Alec Wong <aw685@cornell.edu>
2019-12-07 16:17:44 -0500

    Update import voting pop data. not going to use this though

--- scripts/import_voting_pop_data.R
+++ scripts/import_voting_pop_data.R
@@ -31,4 +31,28 @@ assert_that(
   file.exists('input/a1.xlsx')
 )
 
+# Now import reg rates by state
+
+url = 'https://www2.census.gov/programs-surveys/cps/tables/time-series/voting-historical-time-series/a5b.xlsx'
+
+# Check that the url works
+
+status = curl::curl_fetch_memory(url)
+
+assert_that(
+  status$status_code == 200,
+  typeof(status$content) == 'raw',
+  length(status$content) > 1
+)
+
+# Fetch the data
+
+curl_download(url = url, destfile = 'input/a5b.xlsx')
+
+# Check that the data were downloaded correctly
+
+assert_that(
+  file.exists('input/a5b.xlsx')
+)
+
 # END --------------------------------------------------------------------------
