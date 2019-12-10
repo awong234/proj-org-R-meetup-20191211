8b83a3b05aa48ea5f5c0a352968b26defc718774
Alec Wong <aw685@cornell.edu>
2019-12-09 22:05:59 -0500

    Update analysis

--- scripts/analyze.R
+++ scripts/analyze.R
@@ -54,112 +54,56 @@ dev.off()
 model_collection = tibble(
   'formulas' = list(
     voting_rate ~ year,
-    voting_rate ~ year * state,
+    voting_rate ~ -1 + year * state,
     voting_rate ~ -1 + state + year,
-    voting_rate ~ year + (year | state)
+    voting_rate ~ year + (year | state),
+    voting_rate ~ (1 | state) + (1| year) + year
   ), 
-  'description' = c(
-    'year fixed',
-    'year by state interaction',
-    'state incpt. year fixed',
-    'random intercept by state, random slope by year'
-  ),
+  'description' = as.character(formulas),
   'reg_call' = list(
     lm,
     lm,
     lm,
+    lmer,
     lmer
   )
 )
 
 model_collection = model_collection %>% 
   mutate(model_fit = map2(.x = formulas, .y = reg_call, ~.y(data = voting, formula = .x))) %>% 
-  mutate(AIC = map_dbl(.x = model_fit, .f = AIC)) %>% 
-  arrange(AIC)
+  mutate(AIC = map_dbl(.x = model_fit, .f = AIC))
 
-model_collection %>% 
-  mutate(tidyfit = map(model_fit, tidy)) %>% 
-  unnest(tidyfit)
+saveRDS(model_collection, file = 'output/model_tbl.RDS')
 
 # Work up ----------------------------------------------------------------- 
 
-# Interaction model -- completely separate intercepts / slope 
-summary(mod_interact)
-
-df = augment(mod_interact)
-
-df %>% ggplot() + 
+pick_index = T
+plot_list = model_collection %>% filter(pick_index) %>% pluck('model_fit')
+names(plot_list) = model_collection$description[pick_index]
+map_dfr(.x = plot_list, .f = ~mutate(voting, .fitted = fitted(.x)), .id = 'model') %>% 
+ # filter(state %in% c("New York", "California", "Oklahoma", "North Carolina")) %>%
+ # filter(state == "New York") %>%
+  ggplot() + 
+  geom_line(aes(x = year, y = .fitted, color = model)) + 
   geom_point(aes(x = year, y = voting_rate)) + 
-  geom_line(aes(x = year, y = .fitted)) + 
   scale_x_continuous(breaks = seq(1992, 2016,by=4), labels = yr) +  
   facet_wrap(~state)
-
-
-summary(mod_fx_year)
-
-df = augment(mod_fx_year)
-
-df %>% ggplot() + 
-  geom_point(aes(x = year, y = voting_rate)) + 
-  geom_line(aes(x = year, y = .fitted)) + 
-  scale_x_continuous(breaks = seq(1992, 2016,by=4), labels = yr) +  
-  facet_wrap(~state)
-
-## One line per state, as in geom_smooth()
-
-vote_spl_state = nest(voting, data = c(year, voting_rate))
-
-vote_spl_state = vote_spl_state %>% 
-  mutate(mod_fits = map(data, .f = ~lm(data = .x, formula = voting_rate ~ year)),
-         tidy_stats = map(mod_fits, ~tidy(.x)),
-         augmentation = map(mod_fits, augment))
-
-# By state, what are the effects?
-
-p_int = vote_spl_state %>% unnest(tidy_stats) %>% select(state, term, estimate) %>% 
-  filter(term == "(Intercept)") %>% 
-  ggplot(aes(x = estimate, y = state)) + 
-  geom_point() + 
-  geom_vline(xintercept = 0) + 
-  theme_bw()
-
-p_year = vote_spl_state %>% unnest(tidy_stats) %>% select(state, term, estimate, std.error) %>% 
-  filter(term == "year") %>% 
-  ggplot(aes(x = state, y = estimate)) + 
-  geom_point() + 
-  geom_errorbar(aes(x = state, ymin = -2*std.error + estimate, ymax = 2*std.error + estimate), width = 0) + 
-  geom_hline(yintercept = 0) + 
-  theme_bw() + 
-  coord_flip()
-
-p_resids = vote_spl_state %>% unnest(augmentation) %>% 
-  ggplot(aes(x = year, y = .resid)) + 
-  geom_point() + 
-  geom_hline(yintercept = 0) +
-  facet_wrap(~state) + 
-  theme_bw()
-
-p_int + p_year + vplot_wrap + plot_layout(widths = c(1,1,4), guides = 'collect')
-p_int + p_year + p_resids + plot_layout(widths = c(1,1,4), guides = 'collect')
 
 ## Fit together with random slopes & intercepts state
 
-
-summary(re_mod_year_yearfx)
-
-tidy(re_mod_year_yearfx)
+re_mod_year_yearfx = model_collection %>% 
+  pluck('model_fit', which(model_collection$description == 'voting_rate ~ (1 | state) + (1 | year) + year'))
 
 voting$re_mod_year_yearfx_fit = fitted(re_mod_year_yearfx)
 
+Cairo(1920, 1620, dpi = 150, type = 'png', file = 'img/random_effects_4_fit.png')
 voting %>% ggplot(aes(x = year, y = voting_rate)) +
   geom_point() + 
   geom_line(aes(x = year, y = re_mod_year_yearfx_fit)) + 
   scale_x_continuous(breaks = seq(1992, 2016,by=4), labels = yr)+  
-  facet_wrap(~state)
+  facet_wrap(~state) + 
+  ggtitle('voting_rate ~ (1 | state) + (1 | year) + year\nRandom intercepts for year and state, with year fixed effect')
+dev.off()
 
-aug = augment(re_mod_year_yearfx)
 
-aug %>% ggplot(aes(x = year, y = .resid)) + 
-  geom_point() + 
-  geom_line() + 
-  facet_wrap(~state)
+system(command = 'touch analysis_done.conf')
